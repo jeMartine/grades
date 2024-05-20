de6d94e477dc6a9df98b69b9560672f2713473fd
jeMartine <jorgeesteban464@gmail.com>
2024-05-19 20:49:53 -0500

    add gitignore

--- src/main/java/edu/javeriana/taller3/controlador/ControladorCurso.java
+++ src/main/java/edu/javeriana/taller3/controlador/ControladorCurso.java
@@ -33,58 +33,9 @@ public class ControladorCurso {
     }
 
     @PostMapping("/add")
-    public Mono<ResponseEntity<String>> addCurso(@RequestBody Curso curso) {
-
-        // Verificar si existen los IDs de materia, profesor y estudiante
-    Mono<Boolean> materiaExists = repositorioMateria.existsById(curso.getMateriaId());
-    //Mono<Boolean> profesorExists = repositorioPersona.findProfesorById(curso.getProfesorId(), 'P');
-    //Mono<Boolean> estudianteExists = repositorioPersona.findEstudianteById(curso.getEstudianteId(), 'E');
-
-    // Suscribirse a los Monos y manejar los resultados
-        return materiaExists.flatMap(materiaExistsResult -> {
-        if (!materiaExistsResult) {
-            return Mono.just(ResponseEntity.badRequest().body("La materia especificada no existe."));
-        } else {
-            return profesorExists.flatMap(profesorExistsResult -> {
-                if (!profesorExistsResult) {
-                    return Mono.just(ResponseEntity.badRequest().body("El profesor especificado no existe."));
-                } else {
-                    return estudianteExists.map(estudianteExistsResult -> {
-                        if (!estudianteExistsResult) {
-                            return ResponseEntity.badRequest().body("El estudiante especificado no existe.");
-                        } else {
-                            try {
-                                repositorioCurso.save(curso);
-                                return ResponseEntity.ok("El curso se ha insertado correctamente.");
-                            } catch (Exception e) {
-                                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                        .body("Error al insertar el curso: " + e.getMessage());
-                            }
-                        }
-                    });
-                }
-            });
-        }
-    });
-}
-
-
-
-    Mono<ResponseEntity<Persona>> updatePersonaById(@PathVariable Integer id, @RequestBody Persona persona){
-        return repositorioPersona.findById(id)
-                .flatMap(existingPersona->{
-                    existingPersona.setNombre(persona.getNombre());
-                    existingPersona.setApellido(persona.getApellido());
-                    existingPersona.setCorreo(persona.getCorreo());
-                    existingPersona.setRol(persona.getRol());
-                    return repositorioPersona.save(existingPersona);
-                })
-                .map(updatedPersona -> ResponseEntity.ok(updatedPersona))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    public Mono<Curso> addCurso(@RequestBody Curso curso) {
+        return repositorioCurso.save(curso);
     }
-
-
-
 
 
     @GetMapping("/get/{materiaid}/{profesorid}/{numero}")
